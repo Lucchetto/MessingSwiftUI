061aeb4aca96a102b05d3b7e702ee102c611ea9c
Lucchetto <lucchetto.tie@live.com>
2022-10-27 22:02:36 +0200

    Initial Commit

---
+++ MessingSwiftUI/MessingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessingSwiftUIApp.swift
+//  MessingSwiftUI
+//
+//  Created by Zhenxiang Chen on 27/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MessingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
